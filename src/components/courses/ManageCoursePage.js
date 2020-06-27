af13b57deadd08a5149d4fe838d3b91b7feecd39
w-ainea <aineah28@gmail.com>
2020-06-27 15:51:58 +0300

    created reusable components and a course form for editing a course

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,10 +1,14 @@
-import React, {useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
 import * as authorActions from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
+import { newCourse } from "../../../tools/mockData";
+import CourseForm from "./CourseForm";
 
 function ManageCoursePage({ courses, authors, loadCourses, loadAuthors }) {
+  const [course, setCourse] = useState({ ...props.course });
+
   useEffect(() => {
     if (courses.length === 0) {
       loadCourses().catch((err) => alert(err));
@@ -13,7 +17,7 @@ function ManageCoursePage({ courses, authors, loadCourses, loadAuthors }) {
     if (authors.length === 0) {
       loadAuthors().catch((err) => alert(err));
     }
-  }),[];
+  }, []);
 
   return (
     <>
@@ -28,9 +32,11 @@ ManageCoursePage.propTypes = {
   actions: PropTypes.object.isRequired,
   loadCourses: PropTypes.func.isRequired,
   loadAuthors: PropTypes.func.isRequired,
+  course: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = (state) => ({
+  course: newCourse,
   courses: state.courses,
   authors: state.authors,
 });
