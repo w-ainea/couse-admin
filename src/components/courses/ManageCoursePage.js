1c7a58fe35b0cf6fe2079d6bdf6a188bf681ef9f
w-ainea <aineah28@gmail.com>
2020-06-27 15:22:38 +0300

    implemented object form of mapDispatchToProps

---
+++ src/components/courses/ManageCoursePage.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseActions";
+import * as authorActions from "../../redux/actions/authorActions";
+import PropTypes from "prop-types";
+
+class ManageCoursePage extends React.Component {
+  componentDidMount() {
+    const { courses, authors, loadCourses, loadAuthors } = this.props;
+
+    if (courses.length === 0) {
+      loadCourses().catch((err) => alert(err));
+    }
+
+    if (authors.length === 0) {
+      loadAuthors().catch((err) => alert(err));
+    }
+  }
+  render() {
+    return (
+      <>
+        <h2>Manage Course</h2>
+      </>
+    );
+  }
+}
+
+ManageCoursePage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  authors: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired,
+  loadCourses: PropTypes.func.isRequired,
+  loadAuthors: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  courses: state.courses,
+  authors: state.authors,
+});
+
+// determines which actions are available on props in our component
+const mapDispatchToProps = {
+  loadCourses: courseActions.loadCourses,
+  loadAuthors: authorActions.loadAuthors,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
