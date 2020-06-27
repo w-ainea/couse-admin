a47563e5678058b32b150c61eeef84846d80e55b
w-ainea <aineah28@gmail.com>
2020-06-27 15:34:51 +0300

    implemented useEffect hook

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,13 +1,11 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
 import * as authorActions from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 
-class ManageCoursePage extends React.Component {
-  componentDidMount() {
-    const { courses, authors, loadCourses, loadAuthors } = this.props;
-
+function ManageCoursePage({ courses, authors, loadCourses, loadAuthors }) {
+  useEffect(() => {
     if (courses.length === 0) {
       loadCourses().catch((err) => alert(err));
     }
@@ -15,14 +13,13 @@ class ManageCoursePage extends React.Component {
     if (authors.length === 0) {
       loadAuthors().catch((err) => alert(err));
     }
-  }
-  render() {
-    return (
-      <>
-        <h2>Manage Course</h2>
-      </>
-    );
-  }
+  }),[];
+
+  return (
+    <>
+      <h2>Manage Course</h2>
+    </>
+  );
 }
 
 ManageCoursePage.propTypes = {
